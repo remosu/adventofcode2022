76bb854eaca4d814a22f17cc1f86d916c2380a2f
Silvio René <silvio.rene@gmail.com>
2022-12-04 08:43:20 +0100

    Day 4 part 2
    More copypasting from `pry` ¯\_(ツ)_/¯

--- 4/assignment_pairs.rb
+++ 4/assignment_pairs.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby
 
-p File.readlines("input")
+secction_assignments =
+  File.readlines("input")
       .map { |line| line.strip.split(',').map { |pair| Range.new(*pair.split('-').map(&:to_i)) } }
-      .select { |r1, r2| r1.cover?(r2) || r2.cover?(r1) }
-      .count
+
+p secction_assignments.select { |r1, r2| r1.cover?(r2) || r2.cover?(r1) }
+                      .count
+
+p secction_assignments.select { |r1, r2| r1.cover?(r2.begin) || r2.cover?(r1.begin) }
+                      .count
