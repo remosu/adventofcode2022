e582114f570763f4c1333393a3a8de1b155d0543
Silvio René <silvio.rene@gmail.com>
2022-12-08 14:44:27 +0100

    Day 8 part 1

---
+++ 8/tree_grid.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+i = 0
+j = 0
+t = [-1]
+b = []
+l = -1
+r = []
+
+n = 0
+v = Set.new
+
+File.new('input').each_char do |c|
+  if c == "\n"
+    r.each { |_, p| v.add(p) }
+    l = -1
+    r = []
+    i += 1
+    j = 0
+  else
+    t[j] ||= -1
+    b[j] ||= []
+    c = c.to_i
+
+    if c > t[j]
+      t[j] = c
+      n += 1
+      v.add([i, j])
+    end
+
+    if c > l
+      l = c
+      n += 1
+      v.add([i, j])
+    end
+
+    b[j].delete_if { |x, _| x <= c}
+    b[j] << [c, [i, j]]
+
+    r.delete_if { |x, _| x <= c }
+    r << [c, [i, j]]
+
+    j += 1
+  end
+end
+
+n += b.map(&:count).sum
+b.each do |x|
+  x.each { |_, p| v.add(p) }
+end
+
+p v.count
