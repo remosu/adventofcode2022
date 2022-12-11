bc7dabd2de8ba1dd926ca7f3e6735800b7a9abe8
Silvio René <silvio.rene@gmail.com>
2022-12-11 15:17:52 +0100

    Day 9 part 2

--- 9/rope_bridge.rb
+++ 9/rope_bridge.rb
@@ -2,33 +2,39 @@
 
 require 'set'
 
+DIRECTIONS = {
+  'R' => [0, 1],
+  'L' => [0, -1],
+  'U' => [1, -1],
+  'D' => [1, 1]
+}
+
 motions = File.readlines('input', chomp: true)
 
-h = [0, 0]
-t = [0, 0]
+knots = 10.times.map { [0, 0] }
 tail_positions = Set.new([[0, 0]])
 
 motions.each do |motion|
   direction, steps = motion.split
   steps = steps.to_i
-  case direction
-  when 'R'
-    h[0] += steps
-  when 'L'
-    h[0] -= steps
-  when 'U'
-    h[1] -= steps
-  when 'D'
-    h[1] += steps
-  end
+  axis, d = DIRECTIONS[direction]
 
-  while [0, 1].any? { |i| (h[i] - t[i]).abs > 1 }
-    v = [h[0] - t[0], h[1] - t[1]]
-    [0, 1].each do |i|
-      v[i] = v[i] / v[i].abs unless v[i].zero?
-      t[i] += v[i]
+  steps.times do
+    knots[0][axis] += d
+
+    9.times do |j|
+      h = knots[j]
+      t = knots[j + 1]
+      next if h.zip(t).all? { |x, y| (x - y).abs <= 1 }
+
+      v = [h[0] - t[0], h[1] - t[1]]
+      [0, 1].each do |i|
+        v[i] = v[i] / v[i].abs unless v[i].zero?
+        t[i] += v[i]
+      end
+
     end
-    tail_positions.add([t[0], t[1]])
+    tail_positions.add(knots[-1].dup)
   end
 end
 
